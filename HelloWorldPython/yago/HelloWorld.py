d34762468c25ad47aeaeb02be49a7ae1496021a8
YagoPeixoto <yagopeixoto19@gmail.com>
2016-07-21 13:20:21 -0300

    add ex de fuções basicas

---
+++ HelloWorldPython/yago/HelloWorld.py
@@ -0,0 +1,28 @@
+'''
+Created on 21 de jul de 2016
+
+@author: yago
+'''
+#imprime na tela 
+print("Hello World")
+print("biiirrrl\n")
+
+#testando o \n
+print("hello world \n biiirrll\n")
+
+#colocando numero no meio de texto
+print("aki é",13,"porra")
+
+'''
+isso
+aki 
+é
+só
+um
+comentario
+'''
+
+"""
+aki tambem é só
+um comentario
+"""
