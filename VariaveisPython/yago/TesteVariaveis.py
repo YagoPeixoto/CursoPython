6d73595eb154446c972147288a8d4556974b3ec0
YagoPeixoto <yagopeixoto19@gmail.com>
2016-07-21 14:01:15 -0300

    add ex de variaveis e operadores matematicos em python

---
+++ VariaveisPython/yago/TesteVariaveis.py
@@ -0,0 +1,23 @@
+'''
+Created on 21 de jul de 2016
+
+@author: yago
+'''
+x = 10
+y = 3
+
+print(x, "mais", y, "é 13 poha") 
+
+a,b = 7, 6
+
+print(a + b, "memo caral..")
+
+soma = a+b
+subtracao = a-b
+multiplicacao = a*b
+divisao = a/b
+divisaoInteira = a//b
+resto = a%b
+potencia = b ** 3 #ou b*b*b ou 6*6*6
+
+print(soma, subtracao, multiplicacao, divisao, divisaoInteira, resto, potencia)
