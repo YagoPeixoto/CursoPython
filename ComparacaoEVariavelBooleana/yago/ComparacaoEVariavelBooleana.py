def610256306e84d924e211b0042f2e288236d0e
YagoPeixoto <yagopeixoto19@gmail.com>
2016-07-23 16:22:15 -0300

    add exercicios operadores de comparação

---
+++ ComparacaoEVariavelBooleana/yago/ComparacaoEVariavelBooleana.py
@@ -0,0 +1,13 @@
+'''
+Created on 23 de jul de 2016
+
+@author: yago
+'''
+#Operadores de comparação <, >, <=, >=, ==, !=
+
+idade = (input("Digite sua idade:"))
+idade = int(idade)
+
+podeBeber = idade >= 18
+
+print(podeBeber)
