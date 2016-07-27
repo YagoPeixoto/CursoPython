8c5f1711af3ea9be36acc8ebbb3aafaca501c940
YagoPeixoto <yagopeixoto19@gmail.com>
2016-07-26 23:58:51 -0300

    add comparação multipla ao exercicio de positivo ou negativo

---
+++ Ex_if_else_python/yago/positivoOuNegativo1.py
@@ -0,0 +1,12 @@
+'''
+Created on 24 de jul de 2016
+
+@author: yago
+'''
+num = int(input("digite um numero :"))
+
+if num < 0:
+    print("Esse numero é negativo")
+    
+    
+else: print("Esse numero é positivo")
---
+++ Ex_if_else_python/yago/positivoOuNegativo2.py
@@ -0,0 +1,27 @@
+'''
+Created on 24 de jul de 2016
+
+@author: yago
+'''
+
+'''
+num = int(input("digite um numero :"))
+
+if num < 0:
+    print("Esse numero é negativo")
+    
+    
+else: print("Esse numero é positivo")
+'''
+
+num = int(input("digite um numero :"))
+
+if 1 > num > -1:
+    print("Esse numero é um zero")
+    
+if num > 0: 
+    print("Esse numero é positivo")
+    
+else:
+    if num < 0:
+        print("Esse numero é negativo")
