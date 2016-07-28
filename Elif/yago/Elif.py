7c152883ad7879e3c9ffa6c8ba63cf627548411f
YagoPeixoto <yagopeixoto19@gmail.com>
2016-07-28 14:33:40 -0300

    add correção de  exercicio com elif python

--- Elif/yago/Elif.py
+++ Elif/yago/Elif.py
@@ -7,23 +7,21 @@ num1 = int(input("Digite o primeiro numero:"))
 num2 = int(input("Digite o segundo numero:"))
 num3 = int(input("Digite o terceiro numero:"))
 
-if num1 > num2 > num3:
+
+if num1 >= num2 >= num3:
     print("O maior numero é", num1, "e o menor é", num3)
     
-elif num1 > num3 > num2:
+elif num1 >= num3 >= num2:
     print("O maior numero é", num1, "e o menor é", num2)
     
-elif num2 > num1 > num3:
+elif num2 >= num1 >= num3:
     print("O maior numero é", num2, "e o menor é", num3)
 
-elif num2 > num3 > num1:
+elif num2 >= num3 >= num1:
     print("O maior numero é", num2, "e o menor é", num1)
 
-elif num3 > num2 > num1:
+elif num3 >= num2 >= num1:
     print("O maior numero é", num3, "e o menor é", num1)
 
-elif num3 > num1 > num2:
-    print("O maior numero é", num3, "e o menor é", num2)
-    
 else:
-    print("Você digitou numeros repitidos.")
+    print("O maior numero é", num3, "e o menor é", num2)
