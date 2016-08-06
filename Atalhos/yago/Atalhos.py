e61945858a551a3038e2d4ce78d0a669725ed328
YagoPeixoto <yagopeixoto19@gmail.com>
2016-08-06 14:54:01 -0300

    add exercicio para facilitar programacao com python

---
+++ Atalhos/yago/Atalhos.py
@@ -0,0 +1,18 @@
+'''
+Created on 6 de ago de 2016
+
+@author: yago
+'''
+
+n = int(input("Digite um numero:"))
+a, b = 1, 5
+cont = 0
+while cont < n:
+    print("mostro %i birrr %i" %(a,b))
+    
+    cont += 1
+    
+    #cont += 1 é = a cont = cont + 1
+    #print("mostro %i birrr %i" %(a,b)) é = a     print("mostro", a, "birrr", b)
+
+    
