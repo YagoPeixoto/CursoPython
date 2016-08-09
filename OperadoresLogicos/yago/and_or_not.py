236fa175f3ff355683071720aa255e504a21dc32
YagoPeixoto <yagopeixoto19@gmail.com>
2016-08-08 23:56:37 -0300

    add exercicio operadores python

---
+++ OperadoresLogicos/yago/and_or_not.py
@@ -0,0 +1,29 @@
+'''
+Created on 8 de ago de 2016
+
+@author: yago
+'''
+n1 = int(input("Digite o sua idade:"))
+if n1 >= 65 and n1 <= 16:
+    print("voce pode votar")
+else:
+    print("vc não pode votar")
+    
+    
+    
+n2 = int(input("Digite o sua idade:"))
+if n2 == 16 or n2 == 17:
+    print("voce vota se quiser:")
+else:
+    print("biilllrr")
+    
+    
+print(not False)
+n3 = int(input("Digite o sua idade:"))
+n3 = False
+while not n3:
+    print(n3)
+    n3 = True
+    
+    
+    
