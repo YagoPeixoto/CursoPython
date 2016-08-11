c963c9b61b2abb04ec665c21c880312340eae287
YagoPeixoto <yagopeixoto19@gmail.com>
2016-08-10 23:20:55 -0300

    add exercicio operadores python

---
+++ OperadoresLogicos/yago/and_or.py
@@ -0,0 +1,12 @@
+'''
+Created on 10 de ago de 2016
+
+@author: yago
+'''
+from lib2to3.fixer_util import String
+
+peso =float(input("digite seu peso:"))
+altura = float(input("digite sua altura:"))
+nome = String()
+
+print(nome, peso, altura)
