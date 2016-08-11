275092d254e520cc0cd5f1115a3f652a3c37636a
YagoPeixoto <yagopeixoto19@gmail.com>
2016-08-11 16:05:06 -0300

    add exercicio imc python

--- OperadoresLogicos/yago/and_or.py
+++ OperadoresLogicos/yago/and_or.py
@@ -3,12 +3,21 @@ Created on 10 de ago de 2016
 
 @author: yago
 '''
-from lib2to3.fixer_util import String
 
-
-
-peso =float(input("digite seu peso:"))
+peso = float(input("digite seu peso:"))
 altura = float(input("digite sua altura:"))
-#nome = String("Escreva seu nome:")
 
-print("Seu peso é %d e sua altura é %d" %(altura, peso))
+imc = peso/(altura * altura)
+
+print("Seu peso é %dKg, sua altura é %d e seu IMC é %f" %(peso,altura,imc))
+
+if imc < 17:
+    print("voce esta muito abaixo do peso")
+elif imc >= 17 and imc <= 18.49:
+    print("voce esta abaixo do peso")
+elif imc > 18.49 and imc <= 24.99:
+    print("voce esta dentro do peso")
+elif imc > 24.99 and imc <= 29.99:
+    print("voce esta acima do peso")
+elif imc > 29.99:
+    print("voce ta parecendo um porco de granja...\nseu gordo vai comer direito e fazer uma atividade fisica")
