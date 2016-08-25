9013828b8859dc2e900c2aa0dbbf29d53473eb38
YagoPeixoto <yagopeixoto19@gmail.com>
2016-08-25 13:49:09 -0300

    add exercicio de lista in  python

---
+++ Listas/yago/IntroductionLista.py
@@ -0,0 +1,20 @@
+'''
+Created on 25 de ago de 2016
+
+@author: yago
+'''
+lista = [1,23,48,5,56,6,7,8,7,9,9,9,9,99,9,9,88,9]
+
+print(lista)
+print(lista[2])# imprimir posição 2 da lista obs: a lista começa a contar a partir ta posição 0
+print(lista[-2])# imprimir a segunta posição de tras pra frente
+
+lista += [20]# adicionar uma lista a outra lista
+print(lista)
+
+print(lista[2] + lista[3])#fazer operações matematicas com elementos de posições das listas
+
+lista[2] = 4.4# alterar valores das listas
+print(lista)
+
+
