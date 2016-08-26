7d573138f297d3230e66dc8af64c7528f4263778
YagoPeixoto <yagopeixoto19@gmail.com>
2016-08-26 18:32:51 -0300

    add exercicios count  python

---
+++ LenAndCount/yago/LenAndCount.py
@@ -0,0 +1,20 @@
+'''
+Created on 26 de ago de 2016
+
+@author: yago
+'''
+
+lista = []
+
+num = int(input("Digite um número : "))
+
+while num != 0:
+    lista.append(num)
+    num = int(input("Digite um número: "))
+
+elemento = int(input("Digite o elemento a ser procurado: "))
+
+
+print("O elemento %i aparece %i vezes na sequência."%(elemento, lista.count(elemento)))
+        
+        #o  count conta quantos elementos x tem em uma lista
