a0f05bdfa905c1506b656cbd7c7663abe8cba964
YagoPeixoto <yagopeixoto19@gmail.com>
2016-09-13 15:16:05 -0300

    ex tuplas

---
+++ Tuplas/yago/Tuplas.py
@@ -0,0 +1,23 @@
+'''
+Created on 13 de set de 2016
+
+@author: yago
+'''
+l = [4,3,2,1]# isso é uma lista, em uma lista voce pode mudar os elementos dentro dela
+
+t =(1,2,3,4)# isso é uma tupla, em uma tupla vc não pode mudar os elementos dentro dela
+
+t2 = 1,2,3,4 #isso tbm é uma tupla
+
+j,k = 56,7#isso tbm é tupla
+
+y = (8)#voce esta atribuindo valor a variavel y
+y = (8,)#já aki é uma tupla de um unico elemento e não é uma variavel
+
+a,b,c,d = l
+
+e,f,g,h = t
+
+print(a, h)
+
+#geralmente trabalhamos com tuplas sem saber que que estamos trabalhando com tuplas..
